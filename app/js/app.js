9cd2f7128f43ffdabbf76e4d6a597f49fb515e61
rustane <erustan@vpn41-236.hbs.edu>
2014-06-30 10:57:49 -0400

    changing namespace and cleaning structure.
    Changing to the allston namespace and removing some of the angular seed
    application plumbing that we don’t use.

--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
+  'allston.filters',
+  'allston.directives',
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,12 +1,8 @@
 'use strict';
 
 /* Directives */
-angular.module('myApp.directives', []).
-    directive('appVersion', ['version', function(version) {
-        return function(scope, elm, attrs) {
-            elm.text(version);
-        };
-    }]).directive('numerical', ['$timeout', function($timeout) {
+angular.module('allston.directives', [])
+    .directive('numerical', ['$timeout', function($timeout) {
 
         var formatNumber = function (scope) {
 
